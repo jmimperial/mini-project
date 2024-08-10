e56f92d6361157bf184ccef1a4e27de8cde83cb5
jmimpe <johnmichael.imperial@gmail.com>
2024-08-10 11:26:42 +0800

    no message

---
+++ ismo/src/main/java/com/imperial/ismo/IsmoApplication.java
@@ -0,0 +1,13 @@
+package com.imperial.ismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsmoApplication.class, args);
+	}
+
+}
